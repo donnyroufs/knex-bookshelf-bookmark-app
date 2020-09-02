faaf3867189d3793656dd3b0f78b5c09d78334b3
Donny Roufs <iamchets@yahoo.com>
2020-09-02 19:29:30 +0200

    fixed routes

--- src/api/user/User.routes.js
+++ src/api/user/User.routes.js
@@ -1,15 +1,15 @@
 const express = require("express");
-const app = require("../../../index");
 const Controller = require("./User.controller");
+const userModel = require("./User.model");
 
-const userController = new Controller(app.db);
+const userController = new Controller(userModel);
 
 const router = express.Router();
 
 router.get("/", userController.index);
 router.post("/", userController.create);
-router.get("/:id", userController.index);
-router.put("/:id", userController.index);
+router.get("/:id", userController.show);
+router.put("/:id", userController.update);
 router.delete("/:id", userController.destroy);
 
 module.exports = router;
