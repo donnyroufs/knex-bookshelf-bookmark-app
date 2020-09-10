d23ebf0327a98108b450aeab9031b06f3fc6f6c8
Donny Roufs <iamchets@yahoo.com>
2020-09-10 11:01:11 +0200

    add category and bookmark routes

--- src/api/routes.js
+++ src/api/routes.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const userRoutes = require("./user/User.routes");
 const authRoutes = require("./auth/Auth.routes");
+const categoryRoutes = require("./category/Category.routes");
+const bookmarkRoutes = require("./bookmark/Bookmark.routes");
 
 const router = express.Router();
 
 router.use("/auth", authRoutes);
 router.use("/user", userRoutes);
+router.use("/category", categoryRoutes);
+router.use("/bookmark", bookmarkRoutes);
 
 module.exports = router;
