e7d805302f19076af4aaafb05f70267c6bc10d99
Donny Roufs <iamchets@yahoo.com>
2020-09-11 15:22:10 +0200

    example chetsbull auth

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,8 +1,25 @@
 import Auth from "./services/Auth.service";
+import Chetsbull from "chetsbull";
 
 const dummyAccount = {
   email: "admin@mail.com",
   password: "123456",
 };
 
-Auth.login(dummyAccount);
+new Chetsbull(
+  (view) => ({
+    async login(context) {
+      context.event.preventDefault();
+      if (!Auth.isAuthenticated()) {
+        await Auth.login(dummyAccount);
+        view.status.innerHTML = "You are logged in.";
+        view.login.innerHTML = "Logout";
+      } else {
+        Auth.logout();
+        view.status.innerHTML = "Logged out.";
+        view.login.innerHTML = "Login";
+      }
+    },
+  }),
+  "app"
+);
